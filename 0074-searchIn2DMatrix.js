e60e03e813cfdf283caf96e3773056bfa2bd081f
Lingadurai03 <lingadurai033@gmail.com>
2025-03-30 13:38:50 +0530

    Add: 0074-searchIn2DMatrix

---
+++ 0074-searchIn2DMatrix.js
@@ -0,0 +1,61 @@
+// 74. Search a 2D Matrix
+
+// You are given an m x n integer matrix matrix with the following two properties:
+
+// Each row is sorted in non-decreasing order.
+// The first integer of each row is greater than the last integer of the previous row.
+// Given an integer target, return true if target is in matrix or false otherwise.
+
+// You must write a solution in O(log(m * n)) time complexity.
+
+// Example 1:
+
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+// Output: true
+// Example 2:
+
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+// Output: false
+
+// Constraints:
+
+// m == matrix.length
+// n == matrix[i].length
+// 1 <= m, n <= 100
+// -104 <= matrix[i][j], target <= 104
+
+function search(matrix, target) {
+  for (let i = 0; i < matrix.length; i++) {
+    if (matrix[i][0] <= target && matrix[i][matrix[i].length - 1] >= target) {
+      return binarySearch(matrix[i], target);
+    }
+  }
+  return false;
+}
+
+function binarySearch(arr, k) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor(left + (right - left) / 2);
+
+    if (arr[mid] == k) {
+      return true;
+    }
+    if (arr[mid] > k) {
+      right = mid - 1;
+    } else left = mid + 1;
+  }
+  return false;
+}
+
+console.log(
+  search(
+    [
+      [18, 21, 27],
+      [38, 55, 67],
+    ],
+    55,
+  ),
+);
