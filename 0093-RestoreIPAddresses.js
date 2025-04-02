d2ca412279d4ef1f669f5af30ecd4e00f55089f4
Lingadurai03 <lingadurai033@gmail.com>
2025-04-02 22:31:28 +0530

    Add: 0093-RestoreIPAddresses

---
+++ 0093-RestoreIPAddresses.js
@@ -0,0 +1,53 @@
+// 93. Restore IP Addresses
+// Solved
+// Medium
+// Topics
+// Companies
+// A valid IP address consists of exactly four integers separated by single dots. Each integer is between 0 and 255 (inclusive) and cannot have leading zeros.
+
+// For example, "0.1.2.201" and "192.168.1.1" are valid IP addresses, but "0.011.255.245", "192.168.1.312" and "192.168@1.1" are invalid IP addresses.
+// Given a string s containing only digits, return all possible valid IP addresses that can be formed by inserting dots into s. You are not allowed to reorder or remove any digits in s. You may return the valid IP addresses in any order.
+
+// Example 1:
+
+// Input: s = "25525511135"
+// Output: ["255.255.11.135","255.255.111.35"]
+// Example 2:
+
+// Input: s = "0000"
+// Output: ["0.0.0.0"]
+// Example 3:
+
+// Input: s = "101023"
+// Output: ["1.0.10.23","1.0.102.3","10.1.0.23","10.10.2.3","101.0.2.3"]
+
+// Constraints:
+
+// 1 <= s.length <= 20
+// s consists of digits only.
+
+var restoreIpAddresses = function (s) {
+  let res = [];
+
+  if (s.length > 12) return res;
+
+  function backTrack(idx, dots, ip) {
+    if (dots == 4) {
+      if (idx == s.length) {
+        res.push(ip.slice(0, -1));
+      }
+      return;
+    }
+
+    for (let i = idx; i < Math.min(idx + 3, s.length); i++) {
+      let segment = s.slice(idx, i + 1);
+
+      if (+segment <= 255 && (segment.length == 1 || segment[0] != '0')) {
+        backTrack(i + 1, dots + 1, ip + segment + '.');
+      }
+    }
+  }
+  backTrack(0, 0, '');
+
+  return res;
+};
