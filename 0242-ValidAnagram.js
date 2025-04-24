e86fef0db0ef48129b15872beed494af2e093c2b
Lingadurai03 <lingadurai033@gmail.com>
2025-04-24 06:13:20 +0530

    Add: 242. Valid Anagram

---
+++ 0242-ValidAnagram.js
@@ -0,0 +1,48 @@
+// 242. Valid Anagram
+// Solved
+// Easy
+// Topics
+// Companies
+// YouTube
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+
+// Output: true
+
+// Example 2:
+
+// Input: s = "rat", t = "car"
+
+// Output: false
+
+// Constraints:
+
+// 1 <= s.length, t.length <= 5 * 104
+// s and t consist of lowercase English letters.
+
+// Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+
+function solution(str1, str2) {
+  let n = str1.length;
+  let m = str2.length;
+
+  if (n != m) return false;
+  let freq = new Map();
+
+  for (let i = 0; i < n; i++) {
+    freq.set(str1[i], (freq.get(str1[i]) || 0) + 1);
+    freq.set(str2[i], (freq.get(str2[i]) || 0) - 1);
+  }
+
+  for (let [key, val] of freq) {
+    if (val != 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(solution('anagrram', 'nagaraym'));
